1d897108781345353e780ee3640e115213a22431
ompa <nmaladina@hotmail.com>
2024-08-11 21:46:59 +1200

    added portrait photo to intro page

---
+++ components/Intro.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+
+export default function Intro() {
+  return (
+    <section>
+      <div className="flex items-center justify-center">
+        <div>
+          <Image
+            src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?fit=crop&w=368&h=368&q=100"
+            alt="Ricardo portrait"
+            width="192"
+            height="192"
+            quality="95"
+            priority={true}
+            className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
+          />
+        </div>
+      </div>
+    </section>
+  );
+}
