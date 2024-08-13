f9026022794305400532fe14ad215e810edddecd
ompa <nmaladina@hotmail.com>
2024-08-13 17:52:30 +1200

    added about me page and styling 80%

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -46,46 +46,58 @@ export default function Intro() {
           </motion.span>
         </div>
       </div>
-      <motion.p
+      <motion.h1
         className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-3xl"
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
-        <span className="font-bold">Hello, I'm Omphalus </span> and I'm excited
-        to get back
-        <span className="font-bold"> to web development.</span> I have had
-        previous training and experience in{" "}
-        <span className="font-bold">Frontend development</span> for webb{" "}
-        <span className="italic">sites & apps</span>. My focus is{" "}
-        <span className="underline">React (Next.js)</span>.
-      </motion.p>
+        <span className="font-bold">Hello, I'm Gunter </span> and I'm a
+        <span className="font-bold"> web developer.</span> I've had previous
+        training and experience as a
+        <span className="font-bold"> Fullstack developer</span> for webb
+        <span className="italic">- sites & apps</span>. My focus is in Frontend
+        <span className="underline"> React, (Next.js) and UI design </span>.
+      </motion.h1>
       <div
         className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 
       text-lg font-medium"
       >
         <Link
           href="#contact"
-          className="bg-gray-900 text-white px-7 py-3 
-          flex items-center gap-2 rounded-full"
+          className=" group bg-gray-900 text-white px-7 py-3 
+          flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110
+           hover:bg-gray-950 active:scale-105 transition"
         >
-          Contact me here <BsArrowRight />
+          Contact me here{" "}
+          <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
         </Link>
         <a
-          href=""
-          className="bg-white px-7 py-3 
-          flex items-center gap-2 rounded-full"
+          href="/CV.pdf"
+          download={true}
+          className="group bg-white px-7 py-3 
+          flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 
+          active:scale-105 transition cursor-pointer border border-black/10"
         >
-          Download CV <HiDownload />
+          Download CV
+          <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
         </a>
         <a
-          href="bg-white p-4 text-gray-700
-          flex items-center gap-2 rounded-full"
+          href="https://linkedin.com"
+          target="_blank"
+          className="bg-white p-4 text-gray-700
+          flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15]
+          hover:text-gray-950  
+          active:scale-[1.15] transition cursor-pointer border border-black/10"
         >
           <BsLinkedin />
         </a>
         <a
-          href="bg-white p-4 text-gray-700
-          flex items-center gap-2 text-[1.35rem] rounded-full"
+          href="https://github.com"
+          target="_blank"
+          className="bg-white p-4 text-gray-700
+          flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] 
+          hover:scale-[1.15] hover:text-gray-950 
+          active:scale-[1.15] transition cursor-pointer border border-black/10"
         >
           <FaGithubSquare />
         </a>
