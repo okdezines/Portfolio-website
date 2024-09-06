6ee40729404249fe326f938c1f0217d34705d2f6
ompa <nmaladina@hotmail.com>
2024-09-07 01:06:32 +1200

    just added Contact component to the page and anchor link to the heading

---
+++ components/contact.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import SectionHeading from './section-heading'
+
+
+export default function Contact() {
+    return (
+        <section id='contact' className='scroll-mt-28'>
+
+            <SectionHeading>Contact</SectionHeading>
+
+        </section>
+
+    )
+}
